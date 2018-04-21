d6e2bace6ef0de88f950ae67abafe78249b7b664
mengguangxu <15044163060@qq.com>
2018-04-21 23:51:07 +0800

    second

---
+++ js/js2/js/task2-2.js
@@ -0,0 +1,9 @@
+function back(){
+    var revert=confirm("确定要返回首页么？");
+    if(revert===true){
+        window.location.href="../html/task2-1.html";
+    }
+    else {
+        return null;
+    }
+}
--- js/js2/task2-2.js
+++
@@ -1,4 +0,0 @@
-function back(){
-    window.location.href="../html/task2-1.html";
-    confirm("确定要返回首页么？");
-}
