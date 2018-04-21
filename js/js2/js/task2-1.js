0749776b532c849e89e2be271083e86dc6471018
mengguangxu <15044163060@qq.com>
2018-04-21 22:22:11 +0800

    second

--- js/js2/js/task2-1.js
+++ js/js2/js/task2-1.js
@@ -1,4 +1,4 @@
 function start(){
-    // window.location.href="../html/task2-2.html";
-    window.open('../html/task2-2.html');
+    window.location.href="../html/task2-2.html";//在原窗口打开
+    // window.open ('../html/task2-2.html')在新窗口打开
 }
