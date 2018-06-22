8f604e0bb9a82b569030c148f2c7bf4daa868deb
mengguangxu <15044163060@qq.com>
2018-06-22 17:46:01 +0800

    1

--- js/js6/js/filter.js
+++ js/js6/js/filter.js
@@ -1,11 +1,6 @@
+myApp
 //类型
-myApp.constant('list',{
-    0:'首页banner',
-    1:'找职位banner',
-    2:'找精英banner',
-    3:'行业大图'
-});
-myApp.filter('typeFilter',function(){
+.filter('typeFilter',function(){
     return function(types){
         switch(types){
             case 0:
@@ -18,13 +13,9 @@ myApp.filter('typeFilter',function(){
                 return '行业大图';
         }
     };
-});
+})
 //状态
-myApp.constant('listStatus',{
-    1:'草稿',
-    2:'上线'
-});
-myApp.filter('statusFilter',function(){
+.filter('statusFilter',function(){
     return function(listStatus){
         switch(listStatus){
             case 1:
@@ -33,13 +24,9 @@ myApp.filter('statusFilter',function(){
                 return '上线';
         }
     }
-});
+})
 //上下线
-myApp.constant('down',{
-    1:'上线',
-    2:'下线'
-});
-myApp.filter('downFilter',function(){
+.filter('downFilter',function(){
     return function(down){
         switch(down){
             case 1:
@@ -48,18 +35,9 @@ myApp.filter('downFilter',function(){
                 return '下线';
         }
     }
-});
+})
 //新增，编辑
-myApp.constant('industry1',{
-        0:'移动互联网',
-        1:'电子商务',
-        2:'企业服务',
-        3:'O2O',
-        4:'教育',
-        5:'金融',
-        6:'游戏'
-});
-myApp.filter('articleFilter',function(){
+.filter('articleFilter',function(){
     return function(industry){
         switch(industry){
             case 0:
@@ -79,38 +57,11 @@ myApp.filter('articleFilter',function(){
         }
 
     }
-});
-myApp.filter("contentFilter", function ($sce) {
+})
+.filter("contentFilter", function ($sce) {
     return function (text) {
         return $sce.trustAsHtml(text);
     }
 });
 
-myApp.constant("sidebarList",[
-    {
-        firstLevel: "信息管理",
-        secondLevel:[
-            {title: "公司列表", url: ""},
-            {title: "职位列表", url: ""}
-        ]
-    },
-    {
-        firstLevel: "Article管理",
-        secondLevel:[
-            {title: "Article列表", url: "backstage.list"},
-            {title: "文章管理", url: ""}
-        ]
-    },
-    {
-        firstLevel: "用户管理",
-        secondLevel:[
-            {title: "用户列表", url: ""}
-        ]
-    },
-    {
-        firstLevel: "内容管理",
-        secondLevel:[
-            {title: "视频管理", url: ""}
-        ]
-    }
-]);
+
